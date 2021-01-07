d40676f4699d47aa64f9cca7911f91c1198670c6
moulibheemaneti <moulibheemaneti99@gmail.com>
2021-01-07 12:22:21 +0530

    edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+//edited
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:naya/localization/demo_localization.dart';
@@ -9,73 +10,73 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
-  static void setLocale(BuildContext context,Locale locale){
-    _MyAppState state=context.findAncestorStateOfType< _MyAppState>();
+  static void setLocale(BuildContext context, Locale locale) {
+    _MyAppState state = context.findAncestorStateOfType<_MyAppState>();
     state.setLocale(locale);
   }
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  DatabaseReference ref=FirebaseDatabase.instance.reference();
+  DatabaseReference ref = FirebaseDatabase.instance.reference();
 
-    
   Locale _locale;
-  void setLocale(Locale locale){
+  void setLocale(Locale locale) {
     setState(() {
-      _locale=locale;
+      _locale = locale;
     });
   }
+
   @override
-  void didChangeDependencies(){
-    getLocale().then((locale){
-setState(() {
-  this._locale=locale;
-});
+  void didChangeDependencies() {
+    getLocale().then((locale) {
+      setState(() {
+        this._locale = locale;
+      });
     });
     super.didChangeDependencies();
   }
+
   @override
   Widget build(BuildContext context) {
-   if(_locale==null){
-     return Container(
-       child:Center(
-         child:CircularProgressIndicator(),
-       ),
-     );
-   }
-   else{
+    if (_locale == null) {
+      return Container(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
       return MaterialApp(
-      locale:_locale,
-      supportedLocales: [
-        Locale('en','US'),
-        Locale('hi','IN'),
-        Locale('te','IN'),
-        Locale('ta','IN'),
-        Locale('kn','IN'),
-        Locale('ml','IN'),
-      ],
-      localizationsDelegates: [
-   // ... app-specific localization delegate[s] here
-   DemoLocalization.delegate,
-   GlobalMaterialLocalizations.delegate,
-   GlobalWidgetsLocalizations.delegate,
-   GlobalCupertinoLocalizations.delegate,
- ],
- localeResolutionCallback: (deviceLocale,supportedLocales){
-   for(var loacle in supportedLocales){
-     if(loacle.languageCode==deviceLocale.languageCode && loacle.countryCode==deviceLocale.countryCode){
-       return deviceLocale;
-     }
-
-   }
-   return supportedLocales.first;
- },
-       
-      debugShowCheckedModeBanner: false,
-      home: NGSplashScreen(),
-    );
-   }
+        locale: _locale,
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('hi', 'IN'),
+          Locale('te', 'IN'),
+          Locale('ta', 'IN'),
+          Locale('kn', 'IN'),
+          Locale('ml', 'IN'),
+        ],
+        localizationsDelegates: [
+          // ... app-specific localization delegate[s] here
+          DemoLocalization.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        localeResolutionCallback: (deviceLocale, supportedLocales) {
+          for (var loacle in supportedLocales) {
+            if (loacle.languageCode == deviceLocale.languageCode &&
+                loacle.countryCode == deviceLocale.countryCode) {
+              return deviceLocale;
+            }
+          }
+          return supportedLocales.first;
+        },
+        debugShowCheckedModeBanner: false,
+        home: NGSplashScreen(),
+      );
+    }
   }
 }
